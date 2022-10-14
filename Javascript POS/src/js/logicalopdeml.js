5a4a2f4730f1dd47bceb85affe5786522123bf72
Sandhya Molakala <sandhya.molakala@zemosolabs.com>
2022-10-14 05:56:27 +0530

    corrected spelling

--- Javascript POS/src/js/logicalopdeml.js
+++ Javascript POS/src/js/logicalopdeml.js
@@ -3,11 +3,10 @@ let num1 = 8
 let num2 = 9
 let num3 = 7
 
-console.log("Demonstrating logical operators "+res)
 
 let res = num1 < num2 && num1 < num3
 console.log("Demonstrating && "+res)
 res = num1 < num2 || num1 < num3
 console.log("Demonstrating || "+res)
-consolo.log("Demonstrating ! "+!res)
+console.log("Demonstrating ! "+!res)
 
